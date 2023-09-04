60c494a94f26f96e29dc6654fb5b9add84ce3c29
Nikola Hristov <nklhtv@gmail.com>
2023-09-04 14:57:40 +0300

    Add recommended config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { onlyExportComponents } from "./only-export-components.ts";
 
+export const configs = {
+  recommended: {
+    plugins: ["react-refresh"],
+    rules: {
+      "react-refresh/only-export-components": "warn",
+    },
+  },
+};
+
 export const rules = {
   "only-export-components": onlyExportComponents,
 };
