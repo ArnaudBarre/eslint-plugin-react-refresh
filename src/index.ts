66ebb460d8281bbe3f2ada292107a8c38090b608
Nisharg Shah <nishargshah3101@gmail.com>
2025-01-25 11:46:40 +0000

    fix: added name attribute for eslint config inspector

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { onlyExportComponents } from "./only-export-components.ts";
 export const rules = {
   "only-export-components": onlyExportComponents,
 };
+
 const plugin = { rules };
 
 export const configs = {
   recommended: {
+    name: "react-refresh/recommended",
     plugins: { "react-refresh": plugin },
     rules: { "react-refresh/only-export-components": "error" },
   },
   vite: {
+    name: "react-refresh/vite",
     plugins: { "react-refresh": plugin },
     rules: {
       "react-refresh/only-export-components": [
